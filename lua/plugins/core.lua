7194c9e8be2c6cfe8072f5cfabbef4a40f635ecc
Daniel Lundqvist <daniel.lundqvist84@gmail.com>
2024-11-15 14:56:10 +0100

    add rainbow

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -71,4 +71,20 @@ return {
       },
     },
   },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    specs = {
+      {
+        "catppuccin",
+        optional = true,
+        opts = { integrations = { rainbow_delimiters = true } },
+      },
+    },
+    event = "User",
+    main = "rainbow-delimiters.setup",
+    opts = {},
+  },
 }
